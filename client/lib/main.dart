57ebb165ed049aefd78a32c8b382c1909d9a709a
hailemichael121 <yihunaashe@gmail.com>
2024-12-08 19:54:56 +0300

    added supported languages

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:client/core/theme/theme.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
@@ -19,6 +20,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Zefenic',
       theme: AppTheme.darkThemeMode,
+      locale:
+          const Locale('en', 'US'), // Set the default locale to English (US)
+      supportedLocales: const [
+        Locale('en', 'US'), // English (US)
+        Locale('am',
+            'ET'), // Amharic (Ethiopia) - Add any other languages you support
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
       home: const SignupPage(),
     );
   }
